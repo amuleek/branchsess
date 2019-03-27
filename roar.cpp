39446d152b5aeb29812119bef832a6b77cd00a95
amuleek <amuleeksidhu96@gmail.com>
2019-03-27 13:08:32 +0530

    fifth commit

--- roar.cpp
+++ roar.cpp
@@ -40,3 +40,17 @@ int main() {
    Box1.setLength(6.0);
    Box1.setBreadth(7.0);
    Box1.setHeight(5.0);
+   // box 2 specification
+   Box2.setLength(12.0);
+   Box2.setBreadth(13.0);
+   Box2.setHeight(10.0);
+
+   // volume of box 1
+   volume = Box1.getVolume();
+   cout << "Volume of Box1 : " << volume <<endl;
+
+   // volume of box 2
+   volume = Box2.getVolume();
+   cout << "Volume of Box2 : " << volume <<endl;
+   return 0;
+}
